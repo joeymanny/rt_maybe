6799aa540b3f09355586cfd651ad28ba37d2014b
joeymanny <joeymanny1212@gmail.com>
2024-01-07 20:10:56 -0900

    init

---
+++ src/main.rs
@@ -0,0 +1,234 @@
+const ZOOM_BASE: f32 = 1.3;
+
+const LEN: usize = 15;
+
+use wgpu::{PipelineLayoutDescriptor, RenderPipelineDescriptor, util::DeviceExt};
+use winit::{event::KeyEvent, keyboard::KeyCode};
+fn main(){
+    let event_loop = winit::event_loop::EventLoop::new().unwrap();
+    let primary_size = event_loop.primary_monitor().unwrap().size();
+    // sure hope nobody has a monitor less than 10 pixels 
+    let window = winit::window::WindowBuilder::new()
+        // .with_min_inner_size(winit::dpi::PhysicalSize{width,height,})
+        .with_inner_size(primary_size)
+        .with_title("raytracing")
+        .build(&event_loop)
+        .unwrap();
+    env_logger::init();
+    pollster::block_on(run(event_loop, window));
+}
+
+#[derive(Debug)]
+struct Sphere{
+    position: [f32; 3],
+    radius: f32,
+    color: [f32; 3]
+}
+
+// impl Sphere{
+//     fn as_wgpu_aligned_bytes(&self) -> &[u8]{
+//         &[
+//             // <position>
+//             bytemuck::cast::<f32, [u8 ; 4]>(self.position[0]),
+//                 bytemuck::cast::<f32, [u8 ; 4]>(self.position[1]),
+//                 bytemuck::cast::<f32, [u8 ; 4]>(self.position[2]),
+//                 bytemuck::cast::<f32, [u8 ; 4]>(f32::default()), // 16 bytes - vec3<f32>
+//             // </position>
+//             // <color>
+//             bytemuck::cast::<f32, [u8 ; 4]>(self.color[0]),
+//                 bytemuck::cast::<f32, [u8 ; 4]>(self.color[1]),
+//                 bytemuck::cast::<f32, [u8 ; 4]>(self.color[2]),
+//                 bytemuck::cast::<f32, [u8 ; 4]>(f32::default()), // 16 bytes - vec3<f32>
+//             // </color>
+//             // <radius>
+//             bytemuck::cast::<f32, [u8 ; 4]>(self.radius) // 4 bytes - f32
+//             // </radius>
+//         ].into_iter().flatten().collect::<Box<[u8]>>()
+//         // jesus christ
+//     }
+// }
+
+async fn run(event_loop: winit::event_loop::EventLoop<()>, window: winit::window::Window) {
+    let mut size = window.inner_size();
+    size.width = size.width.max(1);
+    size.height = size.height.max(1);
+    let instance = wgpu::Instance::default();
+    let surface = unsafe { instance.create_surface(&window) }.unwrap();
+    let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions{
+        power_preference: wgpu::PowerPreference::HighPerformance,
+        force_fallback_adapter: false,
+        compatible_surface: Some(&surface),
+    }).await.expect("couldn't find an adapter");
+    let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor{
+            label: None,
+            features: wgpu::Features::empty(),
+            limits: wgpu::Limits::downlevel_defaults()
+                .using_resolution(adapter.limits())
+        },
+        None
+    ).await.expect("couldn't find an adequate device");
+    let mandel_commands: &mut [f32; LEN] = &mut [
+        size.width as f32, size.height as f32, // screen dimensions
+        0.0, 0.0, // linear offset
+        4.0, // zoom
+        1.0, // level of detail (num mandel iterations)
+        0.0, 0.0, 0.0, 0.0,// sphere position (plus padding)
+        1.0, 0.0, 0.0, 0.0, // sphere color (plus padding)
+        0.5, // sphere radius
+    ];
+    let staging_mandel_commands_buffer = device.create_buffer(&wgpu::BufferDescriptor{
+        label: None,
+        size: (std::mem::size_of::<f32>() * LEN) as u64,
+        usage: wgpu::BufferUsages::COPY_SRC | wgpu::BufferUsages::MAP_WRITE,
+        mapped_at_creation: false,
+    });
+    let mandel_commands_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor{
+        label: None,
+        contents: bytemuck::cast_slice(mandel_commands),
+        usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST
+
+    });
+    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor{
+        label: None,
+        entries: &[wgpu::BindGroupLayoutEntry{
+            binding: 0,
+            visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
+            ty: wgpu::BindingType::Buffer{
+                ty: wgpu::BufferBindingType::Uniform,
+                has_dynamic_offset: false,
+                min_binding_size: None,
+            },
+            count: None,
+        }]
+    });
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor{
+        label: None,
+        layout: &bind_group_layout,
+        entries: &[wgpu::BindGroupEntry{
+            binding: 0,
+            resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding{
+                buffer: &mandel_commands_buffer,
+                offset: 0,
+                size: None,
+            })
+        }]
+    });
+    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor{
+        label: None,
+        source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(include_str!("shader.wgsl")))
+    });
+    let pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor{
+        label: None,
+        bind_group_layouts: &[&bind_group_layout],
+        push_constant_ranges: &[],
+    });
+    let swapchain_capabilities = surface.get_capabilities(&adapter);
+    let swapchain_format = swapchain_capabilities.formats[0];
+    let render_pipeline = device.create_render_pipeline(&RenderPipelineDescriptor{
+        label: None,
+        layout: Some(&pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &shader,
+            entry_point: "vs_main",
+            buffers: &[]
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &shader,
+            entry_point: "fs_main",
+            targets: &[Some(swapchain_format.into())]
+        }),
+        primitive: wgpu::PrimitiveState::default(),
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState::default(),
+        multiview: None,
+    });
+    let mut config = wgpu::SurfaceConfiguration{
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: swapchain_format,
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::Fifo,
+        alpha_mode: swapchain_capabilities.alpha_modes[0],
+        view_formats: vec![],
+    };
+    surface.configure(&device, &config);
+
+    event_loop.run(
+    move |event, target|{
+        // so they get cleaned up since run() never returns
+        let _ = (&instance, &adapter, &shader, &pipeline_layout);
+        let mut is_mandel_update = false;
+        if let winit::event::Event::WindowEvent {
+            event, // shadowing
+            window_id: _
+        } = event {
+        match event {
+            winit::event::WindowEvent::Resized(new_size) => {
+                config.width = new_size.width.max(1);
+                config.height = new_size.height.max(1);
+                surface.configure(&device, &config);
+
+                mandel_commands[0] = new_size.width as f32;
+                mandel_commands[1] = new_size.height as f32;
+                is_mandel_update = true;
+
+                window.request_redraw();
+            }
+            winit::event::WindowEvent::RedrawRequested =>{
+                let frame = surface
+                    .get_current_texture()
+                    .expect("coulnd't get next swap chain texture");
+                let view = frame
+                    .texture
+                    .create_view(&wgpu::TextureViewDescriptor::default());
+                let mut encoder =
+                    device.create_command_encoder(&wgpu::CommandEncoderDescriptor{label: None});
+                {
+                    let mut rpass =
+                        encoder.begin_render_pass(&wgpu::RenderPassDescriptor{
+                            label: None,
+                            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                                view: &view,
+                                resolve_target: None,
+                                ops: wgpu::Operations {
+                                    load: wgpu::LoadOp::Clear(wgpu::Color::BLUE),
+                                    store: wgpu::StoreOp::Store,
+                                }
+                            })],
+                            ..Default::default()
+                        });
+                        rpass.set_bind_group(0, &bind_group, &[]);
+                        rpass.set_pipeline(&render_pipeline);
+                        rpass.draw(0..3, 0..2);
+                }
+                queue.submit(Some(encoder.finish()));
+                frame.present();
+            },
+            winit::event::WindowEvent::CloseRequested => target.exit(),
+            _ => (),
+            }
+        }
+        if is_mandel_update{
+            dbg!(&mandel_commands);
+            let (sender, receiver) = flume::bounded(1);
+            let slice = staging_mandel_commands_buffer.slice(..);
+            slice.map_async(wgpu::MapMode::Write, move |v| sender.send(v).unwrap());
+            device.poll(wgpu::Maintain::Wait);
+
+            if let Ok(Ok(())) = receiver.recv(){
+                let mut mapped = slice.get_mapped_range_mut();
+                mapped.clone_from_slice(bytemuck::cast_slice(mandel_commands));
+                drop(mapped);
+                staging_mandel_commands_buffer.unmap();
+                let mut encoder = device.create_command_encoder(&Default::default());
+                encoder.copy_buffer_to_buffer(
+                    &staging_mandel_commands_buffer, 0,
+                    &mandel_commands_buffer, 0,
+                    (std::mem::size_of::<f32>() * LEN) as u64,
+                );
+                queue.submit(Some(encoder.finish()));
+            }
+        }
+    })
+    .unwrap();
+}
---
+++ src/shader.wgsl
@@ -0,0 +1,135 @@
+@group(0) @binding(0) var<uniform> cmd: MandelCommands;
+
+struct MandelCommands{
+    size: vec4<f32>,
+    offset: vec4<f32>,
+    zoom: f32,
+    lod: f32,
+    sphere: Sphere
+}
+struct Sphere{
+    pos: vec4<f32>,
+    color: vec4<f32>,
+    radius: f32,
+}
+
+@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32, @builtin(instance_index) instance_index: u32) -> @builtin(position) vec4<f32> {
+    // let x = f32(i32(in_vertex_index) - 1);
+    // let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
+    var x: f32 = 0.0;
+    var y: f32 = 0.0;
+    // square
+    if in_vertex_index == u32(0){
+        x = -1.;
+        y = -1.;
+    } else if in_vertex_index == u32(1){
+        if instance_index == u32(0){
+            x = -1.;
+            y = 1.;
+        }else{
+            x = 1.;
+            y = -1.;
+        }
+    }else if in_vertex_index == u32(2){
+        x = 1.;
+        y = 1.;
+    }
+
+    // triangle
+    // if in_vertex_index == u32(0){
+    //     x = -1.;
+    //     y = -1.;
+    // }else if in_vertex_index == u32(1){
+    //     x = 0.;
+    //     y = 1.
+    // }else{
+    //     x = 1.;
+    //     y = -1.;
+    // }
+    return vec4<f32>(f32(x), f32(y), 0.0, 1.0);
+}
+
+const zoom_base = 1.3;
+
+var<private> lodf: f32;
+
+const a: f32 = 0.249658;
+const b: f32 = -10.0235;
+const c: f32 = 57.0;
+
+@fragment
+fn fs_main(@builtin(position) pos: vec4<f32>) -> @location(0) vec4<f32> {
+
+    var newpos = pos.xy / cmd.size.xy; // normalize to 0 - 1
+
+    newpos.x = ((newpos.x - 0.5)    // cartesian coordinates
+        * (cmd.size.x / cmd.size.y))// account for aspect ratio
+        * pow(zoom_base, cmd.zoom)  // zoom in
+    ;
+
+    newpos.y = (newpos.y - 0.5)
+        * pow(zoom_base, cmd.zoom)
+    ;
+
+    newpos += cmd.offset.xy;
+
+    lodf = max(8.0, a * cmd.zoom * cmd.zoom + b * cmd.zoom + c); // dynamic level of detail based off zoom
+    lodf *= cmd.lod;    // the actual requested lod also has an effect
+
+    var mand = f32(mandel(Complex(newpos.x, newpos.y)));
+    var mand_val = mand; // how many iterations did it take
+
+    // taking the average of surrounding pixels improves detail at the cost of performance
+    // var unit = vec2(1.0) / cmd.size;
+    // unit.x *= pow(zoom_base, cmd.zoom);
+    // unit.y *= pow(zoom_base, cmd.zoom);
+    // mand += f32(mandel(Complex(newpos.x - unit.x, newpos.y)))
+    //     + f32(mandel(Complex(newpos.x, newpos.y + unit.y)))
+    //     + f32(mandel(Complex(newpos.x + unit.x, newpos.y)))
+    //     + f32(mandel(Complex(newpos.x, newpos.y - unit.y)));
+    // mand /= 5.0;
+
+    mand /= lodf; // normalize to 0 - 1 (see number of loops in mandel() function)
+    // var e: u8 = 1;
+    var col = mix(vec3(0.008, 0.0, 0.08), vec3(.03, 0.0, 0.0), mand_val % 5.0 / 4.0); // what color should it be if outside of set
+    var background = vec3(0.0);
+    col = mix(col, background, step(f32(u32(lodf) - u32(1)), mand_val)); // should it have outside or inside color
+
+    return vec4<f32>(col, 1.0);
+
+}
+
+
+struct Complex{
+    real: f32,
+    imag: f32,
+}
+
+fn mandel_iter(in: Complex, c: Complex) -> Complex {
+    var ret = sqr(in);
+    ret.real += c.real;
+    ret.imag += c.imag;
+    return ret;
+}
+
+fn sqr(in: Complex) -> Complex {
+    return Complex(pow(in.real, 2.0) - pow(in.imag, 2.0), f32(2.0) * in.real * in.imag);
+}
+
+fn abs_sq(in: Complex) -> f32 {
+    return (pow(in.real, 2.0) + pow(in.imag, 2.0));
+}
+
+fn mandel(c: Complex) -> u32 {
+    var z = Complex(0.0, 0.0);
+    var i: u32 = u32(0);
+    while (i < u32(round(lodf))){
+        i += u32(1);
+        z = mandel_iter(z, c);
+        if abs_sq(z) > 4.0 {
+            return i;
+        }
+    }
+    return i;
+}
